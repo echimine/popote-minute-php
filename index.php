a0798339650e723f113cf46059af7fc97f6037b3
echimine <eliott.chiminello@gmail.com>
2025-03-14 15:18:40 +0100

    first commit added index.php(header main footer) + style + folder json + menu.json

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li>
+                coucou
+            </li>
+            <li>
+                coucou
+            </li>
+            <li>
+                coucou
+            </li>
+            <li>
+                coucou
+            </li>
+        </ul>
+    </header>
+    <main>coucou c'est le main</main>
+    <footer>
+        <ul>
+            <li>contact</li>
+        </ul>
+    </footer>
+</body>
+
+</html>
