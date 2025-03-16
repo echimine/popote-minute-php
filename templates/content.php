ca7239c950bd81fcce77747709bb670fc13bd182
basinho <duartebasile6@gmail.com>
2025-03-16 15:17:06 +0100

    Comment all code

--- templates/content.php
+++ templates/content.php
@@ -1,20 +1,22 @@
 <?php 
 
+// Code pour afficher le contenu de la page dynamiquement
 
+// On vérifie si la variable $_GET['page'] existe
 if (isset($_GET["page"])) {
-    $page = $_GET["page"];
+    $page = $_GET["page"]; // Si elle existe, on envoie l'utilisateur sur la page demandée
 } else {
-    $page = 'home';
+    $page = 'home';  // Sinon, on redirige vers la page d'accueil
 }
 
 switch ($page) {
-    case 'home':
-        include('templates/homepage.php');
+    case 'home': // Si ?page=home
+        include('templates/homepage.php'); // On affiche la page d'accueil
         break;
-    case 'menu';
-        include('templates/week.php');
+    case 'menu'; // Si ?page=menu
+        include('templates/week.php'); // On affiche le menu de la semaine
     break;
-    default:
-    include('templates/404.php');
+    default: // Si la page demandée n'existe pas
+    include('templates/404.php'); // On affiche la page 404
     break;
 } 
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,5 @@
 <?php
+// Code pour afficher dynamiquement les pages dans le header
 // Charger le menu dynamique depuis le fichier JSON
 $menu = json_decode(file_get_contents('json/menu.json'), true);
 ?>
@@ -7,7 +8,7 @@ $menu = json_decode(file_get_contents('json/menu.json'), true);
     <nav>
         <ul>
             <?php
-            //création de la liste 
+            //Création de la liste 
             foreach ($menu as $item) {
                 echo "<li><a href='" . $item['link'] . "'>" . $item['name'] . "</a></li>";
             }
